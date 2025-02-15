77e9c7b6c06a96d7621268e0fe6c1edfbec5a951
Muneeb Hyder <muneeb@Muneebs-MacBook-Air.local>
2025-02-15 10:58:09 -0500

    starter for server shi:

---
+++ python_files/server.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template, jsonify
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("../index.html")
+
+# this isnt the correct code, just correct header just as a template
+@app.route('/index', methods = ['POST'])
+def winner_winner():
+    data = request.json
+    wpm = round(data['wpm'])
+    print(data['wins'])
+    print(find_stats(data['name']))
+    wins = find_stats(data['name'])[0] + data['wins']
+    print(wins)
+    update_stats({'wins': wins, 'name': data['name'], 'wpm': wpm})
+    return jsonify({'message': 'congratulations, stats updated'})
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
